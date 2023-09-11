76220edd1873dd1a21c88e2c9a6be7f3dbb4ae53
JuanVimon3 <jvillamonsalve@gmail.com>
2023-09-11 14:08:25 -0500

    add footer changes before traying positioning with display flex

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css';
+
+const Footer = () => (
+  <footer className={styles.container}>
+    <h5>Copyright © 2023 | All rights reserved</h5>
+  </footer>
+);
+
+export default Footer;
