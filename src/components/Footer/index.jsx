f521b4dda9494840564c73b64825529071cba832
JuanVimon3 <jvillamonsalve@gmail.com>
2023-09-20 10:05:09 -0500

    add requested changes in footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import styles from './styles.module.css';
 
 const Footer = () => (
   <footer className={styles.container}>
-    <h5>Copyright © 2023 | All rights reserved</h5>
+    <h5>Copyright {new Date().getFullYear()} | All rights reserved</h5>
   </footer>
 );
 
