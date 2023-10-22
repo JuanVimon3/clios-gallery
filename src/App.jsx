841ae88bf4220b6ef027baf56a0067521425c6c1
JuanVimon3 <jvillamonsalve@gmail.com>
2023-10-22 11:24:17 -0500

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import Header from './components/Header';
 import Home from './containers/Home';
+import ArtWorkDetail, { loader as artWorkLoader } from './containers/ArtWorkDetail';
 import Footer from './components/Footer';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/art-works/:artWorkId',
+    element: <ArtWorkDetail />,
+    loader: artWorkLoader,
+  },
+]);
 
 const App = () => (
   <div>
     <Header />
-    <Home />
+    <RouterProvider router={router} />
     <Footer />
   </div>
 );
