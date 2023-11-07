b71fb9e8b39886dfc5bddf5b4937f671c26d4fbd
JuanVimon3 <jvillamonsalve@gmail.com>
2023-11-07 15:57:13 -0500

    add modificatiosn to process

---
+++ src/containers/About/index.jsx
@@ -0,0 +1,14 @@
+import styles from './styles.module.css';
+
+const About = () => (
+  <div className={styles.container}>
+    <h1 className={styles.title}> Hello </h1>
+    <h2>
+      Dedicated and enthusiastic Developer Trainee with a passion for technology and a strong foundation in web
+      development. I am eager to contribute my skills to a project and learn continuously in a dynamic software
+      development environment.
+    </h2>
+  </div>
+);
+
+export default About;
