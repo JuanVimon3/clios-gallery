a282fce63fbf10821391c7a3f573ad9e5abee8b9
Gustavo Lopez <glopez@emocha.com>
2023-12-15 10:19:55 -0500

    Updates

--- src/components/Footer/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './styles.module.css';
-
-const Footer = () => (
-  <footer className={styles.container}>
-    <h5>Copyright {new Date().getFullYear()} | All rights reserved</h5>
-  </footer>
-);
-
-export default Footer;
