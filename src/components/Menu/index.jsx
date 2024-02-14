0fa6fbc46285e5c0f9c9540aaf3f69af6694f111
JuanVimon3 <jvillamonsalve@gmail.com>
2024-02-14 15:03:06 -0500

    add some modifications and M-UI fotter component

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-unresolved */
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
@@ -8,57 +7,38 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 import Toolbar from '@mui/material/Toolbar';
 
 const pages = [<Link to="/">Home</Link>, <Link to="/exhibitions/">Exhibitions</Link>, <Link to="/about/">About</Link>];
 
 const MenuComponent = () => {
   const [anchorElNav, setAnchorElNav] = useState(null);
-  const [anchorElUser, setAnchorElUser] = useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
   };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
-  const settings = ['Setting 1', 'Setting 2', 'Setting 3'];
-
   return (
     <AppBar position="static">
       <Container maxWidth="x1">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          />
-
+          <Box sx={{ display: { xs: 'none', md: 'flex' }, nav: { textDecoration: 'none' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block', textDecoration: 'none' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <IconButton
               size="large"
@@ -77,6 +57,7 @@ const MenuComponent = () => {
                 vertical: 'bottom',
                 horizontal: 'left',
               }}
+              autoFocus={Boolean}
               keepMounted
               transformOrigin={{
                 vertical: 'top',
@@ -95,14 +76,12 @@ const MenuComponent = () => {
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
           <Typography
             variant="h6"
             noWrap
             sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
+              flexGrow: 0.7,
+              textAlign: 'center',
               fontWeight: 700,
               letterSpacing: '.3rem',
               color: 'inherit',
@@ -111,44 +90,6 @@ const MenuComponent = () => {
           >
             Clio`s Gallery
           </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button key={page} onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }}>
-                {page}
-              </Button>
-            ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
         </Toolbar>
       </Container>
     </AppBar>
@@ -156,17 +97,3 @@ const MenuComponent = () => {
 };
 
 export default MenuComponent;
-
-/* <nav className={styles.nav}>
-<ul className={styles.ul}>
-<li>
-  <Link to="/">Home</Link>
-</li>
-<li>
-  <Link to="/exhibitions/">Exhibitions</Link>
-</li>
-<li>
-  <Link to="/about/">About</Link>
-</li>
-</ul>
-</nav> */
